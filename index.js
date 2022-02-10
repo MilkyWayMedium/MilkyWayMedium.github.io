b7a5c72153d5409683d15e43d60eb8770b274d91
GarrettHaines <ghaines1@emich.edu>
2022-02-10 00:25:02 -0500

    Connected Google Firebase -GH

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const container = document.getElementById('container');
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB4L71q-5qsAD2RbzYkPI8S66t1-gPmLlc",
+  authDomain: "milkywaymedium.firebaseapp.com",
+  projectId: "milkywaymedium",
+  storageBucket: "milkywaymedium.appspot.com",
+  messagingSenderId: "165659589186",
+  appId: "1:165659589186:web:14f35c3010b2e5600c0bdb",
+  measurementId: "G-WGKXQT89RQ"
+};
+
+
 displayContent();
 
 function displayContent() {
