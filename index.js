49bede056a9fd1073028228dd949af324ce3dbe6
hcook17 <hcook6@emich.edu>
2022-02-15 12:43:25 -0500

    Configuring Firebase setup -GH

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const container = document.getElementById('container');
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB4L71q-5qsAD2RbzYkPI8S66t1-gPmLlc",
   authDomain: "milkywaymedium.firebaseapp.com",
+  databaseURL: "https://milkywaymedium-default-rtdb.firebaseio.com",
   projectId: "milkywaymedium",
   storageBucket: "milkywaymedium.appspot.com",
   messagingSenderId: "165659589186",
   appId: "1:165659589186:web:14f35c3010b2e5600c0bdb",
   measurementId: "G-WGKXQT89RQ"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 displayContent();
