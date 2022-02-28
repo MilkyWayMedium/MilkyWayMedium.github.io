c5838d090d52a51e8bc6a3fce4e3499012e6a241
hcook17 <hcook6@emich.edu>
2022-02-27 22:02:23 -0500

    Refactoring with Webpack -GH

--- components/scripts/bundle.js
+++
@@ -1,6 +0,0 @@
-const uniq = require('uniq');
-const jquery = require('jquery');
-
-var data = [1, 2, 3, 4];
-
-console.log(uniq(data));
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { initializeApp } from 'firebase/app'
+import {
+	getAuth
+} from 'firebase/auth'
+import {
+	getFirestore, collection, getDocs
+} from 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB4L71q-5qsAD2RbzYkPI8S66t1-gPmLlc",
+  authDomain: "milkywaymedium.firebaseapp.com",
+  databaseURL: "https://milkywaymedium-default-rtdb.firebaseio.com",
+  projectId: "milkywaymedium",
+  storageBucket: "milkywaymedium.appspot.com",
+  messagingSenderId: "165659589186",
+  appId: "1:165659589186:web:14f35c3010b2e5600c0bdb",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Initialize Auth
+
+// Initialize Firestore
+const db = getFirestore()
+const colRef = collection(db, 'books')
+getDocs(colRef)
+	.then((snapshot) => {
+		console.log(snapshot.docs)
+	})
