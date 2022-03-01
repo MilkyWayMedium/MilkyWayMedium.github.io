47aeef29652a930edcda55f238e89df8bece4632
hcook17 <hcook6@emich.edu>
2022-02-28 19:03:16 -0500

    Adding video -GH

--- src/post.js
+++ src/post.js
@@ -8,6 +8,10 @@ function displayContent() {
 	
 	postElement.innerHTML = `
 		<p class="text">
+				<iframe id="ytplayer" type="text/html" width="540" height="303.75"
+					src="https://www.youtube.com/embed/hXaPGlkd5do?autoplay=1&fs=0&loop=1&modestbranding=1&playsinline=1&color=white&iv_load_policy=3"
+					frameborder="0" allowfullscreen />
+				
 				Set the vibe in a flash.
 				<br><br>
 				This site is currently under construction.
