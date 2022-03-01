9bbda87bc82a7c923d63ac4c8c67ecce3f45eea0
hcook17 <hcook6@emich.edu>
2022-02-28 20:00:02 -0500

    Added videos -GH

--- src/post.js
+++ src/post.js
@@ -5,10 +5,13 @@ displayContent();
 function displayContent() {
 	const postElement = document.createElement('div');
 	postElement.classList.add('block-content');
+	const videos = ["Aki1Xn36eJ8", "hXaPGlkd5do", "niG3YMU6jFk", "wycjnCCgUes", "2EIeUlvHAiM"];
+	var randomVideo = videos[Math.floor(Math.random()*videos.length)];
+	console.log(randomVideo);
 	
 	postElement.innerHTML = `
 		<iframe id="ytplayer" type="text/html" width="540" height="303.75"
-					src="https://www.youtube.com/embed/Aki1Xn36eJ8?autoplay=1&mute=1&fs=0&loop=1&modestbranding=1&playsinline=1&color=white&iv_load_policy=3"
+					src="https://www.youtube.com/embed/` + randomVideo +`?autoplay=1&mute=1&fs=0&loop=1&modestbranding=1&playsinline=1&color=white&iv_load_policy=3"
 					frameborder="0" allowfullscreen></iframe>
 		<p class="text" style="margin-bottom: -5px; text-align: center;">
 				Set the vibe in a flash. This site is currently under construction.
